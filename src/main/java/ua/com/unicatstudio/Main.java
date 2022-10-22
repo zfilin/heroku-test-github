de6733ed39aa7098530ae6e3295899474180ecbf
zfilin <me@zfilin.org.ua>
2022-10-22 18:14:52 +0300

    init

--- src/main/java/ua/com/unicatstudio/Main.java
+++ src/main/java/ua/com/unicatstudio/Main.java
@@ -2,6 +2,6 @@ package ua.com.unicatstudio;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello github!");
     }
 }
