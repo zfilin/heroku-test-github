d5a49687d6830c9f8445d788e8944fd0f1401477
zfilin <me@zfilin.org.ua>
2022-10-22 20:15:42 +0300

    jgit

--- src/main/java/ua/com/unicatstudio/Main.java
+++ src/main/java/ua/com/unicatstudio/Main.java
@@ -1,9 +1,29 @@
 package ua.com.unicatstudio;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, GitAPIException {
 
+        if(args.length < 1) {
+            System.out.println("args uri");
+            System.exit(1);
+        }
 
+        String repoURI = args[0];
+
+        String repoDir = Files.createTempDirectory("githubActivityDraw").toFile().getAbsolutePath();
+        System.out.println(repoDir);
+
+        Git.cloneRepository()
+                .setURI(repoURI)
+                .setDirectory(new File(repoDir))
+                .call();
 
     }
 }
